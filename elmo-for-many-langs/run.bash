3c8a41d72ca1f70255d4b4edeed6c76a0a440672
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-01-06 22:29:20 +0530

    added changed elmo formany langs script

--- elmo-for-many-langs/run.bash
+++ elmo-for-many-langs/run.bash
@@ -1,5 +1,5 @@
 python -m elmoformanylangs.biLM train \
-    --train_path twitter_750mb_en_data.txt \
+    --train_path twitter_750mb_en_data.raw \
     --config_path config.json \
     --model model \
     --optimizer adam \
@@ -10,4 +10,4 @@ python -m elmoformanylangs.biLM train \
     --min_count 3 \
     --max_epoch 10 \
     --batch_size 32 \
-    --gpu 1
+    --gpu 0
